0640841ae19d05c8c0ef5e5c0e3fece75ca31121
unknown <chg8778@gmail.com>
2022-02-02 22:35:16 -0500

    shortened code

--- index.js
+++ index.js
@@ -24,34 +24,35 @@ function pick_numbers(pick_num, max_num){
 	return _array;
 }
 
+function build_html(_array = []){
+	text_result = "";
+	if(_array.length > 0){
+		_array.forEach(function (item, index) {
+			text_result += '<tr><td>' + item + '</td></tr>';
+		});
+	}
+	document.getElementById("numbers").innerHTML = text_result;
+}
+
 var selection = document.getElementById('selection');
 
 selection.onchange = function(){
 	var pick_number = selection.options[selection.selectedIndex].getAttribute('pick_number');
 	var max_number = selection.options[selection.selectedIndex].getAttribute('max_number');
 	
-	text_result = "";
 	result_array = [];
 	
 	if(pick_number > 5){
-		// result_array = pick_numbers(pick_number, max_number);
-		pick_numbers(pick_number, max_number).forEach(function (item, index) {
-			text_result += '<tr><td>' + item + '</td></tr>';
-		});
-		document.getElementById("numbers").innerHTML = text_result;
+		build_html(pick_numbers(pick_number, max_number));
 		document.getElementById("numbers").style.visibility = "visible";
 
 	} else if(pick_number == 5) {
-		result_array = pick_numbers(pick_number, max_number);
-		result_array.push("<br>", '<a>'+getRndInteger(1, 7)+'</a>');
-		result_array.forEach(function (item, index) {
-			text_result += '<tr><td>' + item + '</td></tr>';
-		});
-		document.getElementById("numbers").innerHTML = text_result;
+
+		build_html(pick_numbers(pick_number, max_number).concat("<br>", '<div class="grandnumber">'+getRndInteger(1, 7)+'</div>'));
 		document.getElementById("numbers").style.visibility = "visible";
 
 	} else {
-		document.getElementById("numbers").innerHTML = text_result;
+		build_html();
 		document.getElementById("numbers").style.visibility = "hidden";
 	}
 };
